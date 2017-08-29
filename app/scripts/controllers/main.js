a0973ff6146b2302484287e16b429e40463edef8
UMROOT\jamzrose <jamzrose@umich.edu>
2017-08-29 13:55:54 -0400

    new files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+* @ngdoc function
+* @name movieMainiaApp.controller:ManinCtrl
+* @description
+* # MainCtrl
+* Controller of the movieMainiaApp
+*/
+angular.module('MovieManiaApp')
+ .controller('MainCtrl', funtion ($scope) {
+ 	$scope.ttitle = 'Thank you for following this course';
+ });
