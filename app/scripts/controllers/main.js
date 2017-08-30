4dd6e533c96135f8cc764f9b5a964b307cedffa8
UMROOT\jamzrose <jamzrose@umich.edu>
2017-08-30 13:11:11 -0400

    no notes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,12 @@
 * Controller of the movieMainiaApp
 */
 angular.module('movieManiaApp')
- .controller('MainCtrl', funtion ($scope) {
- 	$scope.title = 'Thank you for following this course';
+ .controller('MainCtrl', funtion($scope) {
+ 	// var movie = {
+ 	// 	title: 'Ace Drummond',
+ 	// 	image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyNzg3MzcyNF5BMl5BanBnXkFtZTcwNjQ2NTQyMQ@@._V1_UY268_CR2,0,182,268_AL_.jpg',
+ 	// 	description: 'This 13 chapter serial is on the comic strip characters'
+ 	$scope.title = 'Thank you';
+ 	};
+ 	$scope.movie = movie;
  });
